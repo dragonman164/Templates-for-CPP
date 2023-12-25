9ad9fe3c2437cb1fe8f1a416c7b16fbfcde0fd70
SANIDHIYA <56438908+dragonman164@users.noreply.github.com>
2023-12-25 22:36:36 +0530

    Update SCC.cpp

--- SCC.cpp
+++ SCC.cpp
@@ -1,52 +1,69 @@
 /* God is Great !
-    Author : SANIDHIYA*/
+    Author : SANIDHIYA */
 #include<bits/stdc++.h> 
 using namespace std;
-const int mod = 1e9 + 7;
-void solve()
-{
-    // dp[i] number of ways to reach i from 1
-    int n, m;
-    cin >> n >> m;
-    vector<int> adj[n + 1],back_edge[n + 1];
-    map<int,int> deg;
- 
-    for(int i = 0; i < m; i++ ){
-        int u, v;
-        cin >> u >> v;
-        adj[u].push_back(v);
-        back_edge[v].push_back(u);
-        deg[v]++;
- 
-    }
-    vector<int> dp(n + 1, 0);
-    dp[1] = 1;
-    vector<bool> vis(n + 1, false);
-    queue<int> temp;
-        for(int i = 1; i <= n; i++ ){
-        if(deg[i] == 0) {
-            temp.push(i);
-            vis[i] = true;
+const int MAX = 1e5 + 5;
+vector<int> adj[MAX],adjt[MAX];
+stack<int> topo;
+vector<bool> vis1(MAX,false),vis2(MAX, false);
+void dfs(int root){
+    for(auto elem : adj[root]){
+        if(!vis1[elem]){
+            vis1[elem] = true;
+            dfs(elem);
         }
-    }
+    }    
+    topo.push(root);
+}
+vector<int> scc(int curr){
+    queue<int> temp;
+    temp.push(curr);
+    vector<int> ans;
+    ans.push_back(curr);
     while(!temp.empty()){
         int curr = temp.front();
         temp.pop();
-        for(auto elem : back_edge[curr]){
-            dp[curr] = (dp[curr]%mod + dp[elem]%mod)%mod;
-        }
-        for(auto elem : adj[curr]){
-            if(!vis[elem]){
-                deg[elem]--;
-                if(deg[elem] == 0) {
-                    vis[elem] = true;
-                    temp.push(elem);
-                }
-           
+        for(auto elem : adjt[curr]){
+            if(!vis2[elem]){
+                vis2[elem] = true;
+                ans.push_back(elem);
+                temp.push(elem);
             }
         }
     }
-    cout<<dp[n]<<"\n";
+    return ans;
+}
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    for(int i = 1; i <= m; i++ ){
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adjt[v].push_back(u);
+    }
+    for(int i = 1; i <= n ; i++ ){
+        if(!vis1[i]){
+            vis1[i] = true;
+            dfs(i);
+        }
+    }
+    vector<vector<int>> sccs;
+    while(!topo.empty()){
+        int i = topo.top();
+        topo.pop();
+        if(!vis2[i]){
+            vis2[i] = true;
+            sccs.push_back(scc(i));
+        }
+    }
+    if(sccs.size() == 1) cout<<"YES\n";
+    else{
+        cout<<"NO\n";
+        cout<<sccs[1][0]<<" "<<sccs[0][0]<<"\n";
+    }
+ 
  
  
 }
